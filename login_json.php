6b6f3a0a1201ae5d18539a4a7e1d907cb2255faf
GuillermoBlanco9 <60670141+GuillermoBlanco9@users.noreply.github.com>
2020-11-14 20:16:46 +0100

    Login reconoce usuario
    pero no hay redireccion ...

--- login_json.php
+++ login_json.php
@@ -2,13 +2,9 @@
 require_once 'db.php'; 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {   
     $usu = check_user($_POST['user'], $_POST['password']);
-	if($usu===false){
-		$err = true;
-		$user = $_POST['user'];
-	}else{
-		session_start();
-		$_SESSION['user'] = $usu;
-		header("Location: main.php");
-		return;
-	}	
+	if($usu===FALSE){ 
+        echo "FALSE"; 
+    }else{         
+        echo "TRUE";    
+    }  	
 } 
