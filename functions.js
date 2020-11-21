ece126cab81030e8b4ebd2711486eeb443e62979
bernardomft <56193745+bernardomft@users.noreply.github.com>
2020-11-21 17:29:10 +0100

    fecha formateada a data time

--- functions.js
+++ functions.js
@@ -159,17 +159,13 @@ function sendMessage() {
     var msg = document.getElementById('input_msg').value;
     var tituloInnerHTML = document.getElementById('titulo_').innerHTML;
     var currentUser = tituloInnerHTML.substring(9, tituloInnerHTML.length);
-
-
     var  time = new Date().toUTCString().slice(0,-4);
     time=time.substring(5,time.length);
-
-    console.log(time);
+    var date=new Date().toISOString().slice(0, 19).replace('T', ' ');
+    console.log(date);
+    /*console.log(time);
     console.log(msg);
-    console.log(currentUser);
-
-
-    /*
+    console.log(currentUser);*/
     if (!(msg=='')) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
@@ -182,7 +178,6 @@ function sendMessage() {
                 }
             }
         }
-
         var params = "currentUser=" + currentUser + "&user=" + userGlobal + "&body=" + msg + "&time=";
         xhttp.open("POST", "load_chats_json.php", true);
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -191,5 +186,4 @@ function sendMessage() {
     } else {
         return false;
     }
-*/
 }
