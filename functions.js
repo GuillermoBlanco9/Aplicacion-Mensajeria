f61517597c1e9a0abd0ae6a672c7cc9c3ec61c9c
bernardomft <56193745+bernardomft@users.noreply.github.com>
2020-11-30 18:31:49 +0100

    Revert "Update functions.js"
    This reverts commit 0c92f8a4bc2fccf9362916602c457e81278e54f6.

--- functions.js
+++ functions.js
@@ -209,6 +209,7 @@ function onClick() {
                 newChat = 0;
                 newChatUser = 0;
                 //metodo para sacar el div con los chats
+                console.log(this.responseText);
                 cargarConversacion(JSON.parse(this.responseText));
                 intervalConversation = setInterval(updateConver, 1500);
             }
@@ -226,7 +227,7 @@ function updateRead(currentUser) {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             if (this.responseText === "FALSE") {
-                //alert("Actualizado");
+                alert("Actualizado");
             } else {
                 alert("No actualizado");
             }
@@ -324,7 +325,7 @@ function addFriends() {
             if (this.responseText === "FALSE") {
                 alert("No existe el usuario");
             } else {
-                console.log('Por aqui');
+                //console.log('Por aqui');
                 clearInterval(intervalChats);
                 //console.log(intervalReadArray);
                 for (var i = 0; i < intervalReadArray.length; i++)
@@ -357,7 +358,7 @@ function deleteChats() {
 }
 
 function sendFirstMessage(user, currentUserNew) {
-    var msg = 'primer mensaje de ' + currentUserNew + ' a ' + user;
+    var msg = 'asdfgh1234';
     var date = new Date().toISOString().slice(0, 19).replace('T', ' ');
     if (!(msg == '')) {
         var xhttp = new XMLHttpRequest();
@@ -380,17 +381,6 @@ function sendFirstMessage(user, currentUserNew) {
     }
 }
 
-//mostrar el perfil
-
-function cargarPerfil()
-{
-    clearInterval(intervalConversation);
-    while (document.getElementById('conver_id').firstChild)
-        document.getElementById('conver_id').removeChild(document.getElementById('conver_id').firstChild);
-        
-}
-
-
 //boton para mostrar el sign_up
 
 function mostrarForm() {
@@ -408,7 +398,7 @@ function difusionList() {
     var msg = window.prompt('What do you want to tell?');
     console.log(arrayUsers);
     for(var i = 0; i < arrayUsers.length; i++){
-        sendDifList(arrayUsers[i], msg)
+        sendDifList(arrayUsers[i], msg);
     }
 }
 
@@ -428,12 +418,8 @@ function sendDifList(username, msg) {
                 //console.log(intervalReadArray);
                 sendFirstMessage(username, currentUser);
                 sendFirstMessage(currentUser, username);
-                sendDifusionMsg(username, msg);
+                sendDifusionMsg(username,"***DIFUSSION MSG*** " +  msg  + " ***DIFUSSION MSG***");
                 deleteChats();
-                chatGlobal.push(username);
-                console.log(chatGlobal);
-                newChat = 1;
-                newChatUser = username;
                 intervalChats = setInterval(cargarChats, 1500, currentUser);
             }
         }
@@ -449,16 +435,6 @@ function sendDifusionMsg(username, msg){
     var date = new Date().toISOString().slice(0, 19).replace('T', ' ');
     if (!(msg == '')) {
         var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                if (this.responseText === "FALSE") {
-                    alert("No manda mensaje");
-                } /*else {
-                    updateConver();
-                    document.getElementById('input_msg').value = "";
-                }*/
-            }
-        }
         var params = "currentUser=" + currentUser + "&user=" + username + "&body=" + msg + "&time=" + date;
         xhttp.open("POST", "send_msg_json.php", true);
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
