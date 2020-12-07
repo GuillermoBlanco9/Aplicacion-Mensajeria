42f0877de472f8078905914bf3f7a8d5d99ebe17
bernardomft <56193745+bernardomft@users.noreply.github.com>
2020-12-07 22:03:18 +0100

    sesiones mas o menos funciona. Hay que depurar mas

--- chek_read.php
+++ chek_read.php
@@ -1,14 +1,16 @@
 <?php 
 require_once 'db.php';
 require 'sessions_json.php';
-if(!check_session()) return;
-if ($_SERVER["REQUEST_METHOD"] == "POST") {   
-    $check = check_read($_POST['currentUser'], $_POST['user']);
-	if($check===FALSE){ 
-        echo "FALSE"; 
-        return;
-    }else{   
-        echo 'TRUE';
-        return;
-    }  	
-} 
+if(check_session() != "FALSE"){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {   
+        $check = check_read($_POST['currentUser'], $_POST['user']);
+        if($check===FALSE){ 
+            echo "FALSE"; 
+            return;
+        }else{   
+            echo 'TRUE';
+            return;
+        }  	
+    } 
+}
+
--- update_read.php
+++ update_read.php
@@ -1,12 +1,13 @@
 <?php 
 require_once 'db.php';
 require 'sessions_json.php';
-if(!check_session()) return; 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {   
-    $check = update_read($_POST['currentUser'], $_POST['user']);
-	if($check===FALSE){ 
-        echo "FALSE"; 
-    }else{   
-        echo 'TRUE';
-    }  	
-} 
+if(check_session() != "FALSE"){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {   
+        $check = update_read($_POST['currentUser'], $_POST['user']);
+        if($check===FALSE){ 
+            echo "FALSE"; 
+        }else{   
+            echo 'TRUE';
+        }  	
+    } 
+}
